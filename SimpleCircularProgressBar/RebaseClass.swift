2c6feb66bd6f951d66e0edf89957108a8244e524
duypd <leeprohacker@gmail.com>
2019-03-07 12:29:03 +0700

    commit add progress foreground

---
+++ SimpleCircularProgressBar/RebaseClass.swift
@@ -0,0 +1,16 @@
+//
+//  RebaseClass.swift
+//  SimpleCircularProgressBar
+//
+//  Created by Leeprohacker on 3/7/19.
+//  Copyright © 2019 Leeprohacker. All rights reserved.
+//
+
+import Foundation
+
+
+class RebaseClass {
+    
+    var rebase: String = "rebase"
+    
+}
